c9b2ebefb49a4995eaf8638da3b3b40e19bd37e1
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2024-07-15 21:20:57 +0300

    ICacheService eklendi.

--- src/MyTemplate.Application/ServiceRegistiration.cs
+++ src/MyTemplate.Application/ServiceRegistiration.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using Common;
+using Common.Interfaces.Services;
+using Common.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,5 +12,14 @@ public static class ServiceRegistiration
     public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCommonServices(Assembly.GetExecutingAssembly(), configuration);
+
+        services.AddControllers()
+       .AddNewtonsoftJson(opt =>
+       {
+           opt.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+           //opt.SerializerSettings.ContractResolver = new DefaultContractResolver();
+       });
+
+        services.AddScoped<ICacheService, RedisService>();
     }
 }
